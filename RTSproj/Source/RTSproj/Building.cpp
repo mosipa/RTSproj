bf0c087fca4ca9434797a34e09e96722a6e2b4ac
DESKTOP-L8UUJ53\mateu <manisiek@gmail.com>
2019-01-17 21:16:44 +0100

    RTS74 added OnComponentOverlapBegin and OnOverlapBegin method to Building class

--- RTSproj/Source/RTSproj/Building.cpp
+++ RTSproj/Source/RTSproj/Building.cpp
@@ -14,6 +14,9 @@ ABuilding::ABuilding()
 	CollisionComponent->InitCapsuleSize(75.f, 100.f);
 	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
 	CollisionComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ABuilding::OnOverlapBegin);
+
 	SetRootComponent(Cast<USceneComponent>(CollisionComponent));
 
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("BaseMesh"));
@@ -34,3 +37,7 @@ void ABuilding::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void ABuilding::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s"), *(OtherActor->GetName()));
+}
--- RTSproj/Source/RTSproj/Building.h
+++ RTSproj/Source/RTSproj/Building.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	ABuilding();
 
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
